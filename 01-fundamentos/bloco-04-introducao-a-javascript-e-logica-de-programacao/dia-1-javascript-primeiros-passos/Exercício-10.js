4d9f0a2e0849a9416e45c53a0b9b1e91d2e28dd1
Walace <walacewab@gmail.com>
2021-07-29 18:34:19 -0300

    Adicionar correçoes

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/Exercício-10.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/Exercício-10.js
@@ -1,10 +1,10 @@
-let custoDoProduto = (04 * 0.2);
+let custoDoProduto = (4 * 0.2);
 let valorDeVenda = 16;
 let lucro = valorDeVenda - custoDoProduto;
 
 if (lucro * 1000){
     console.log("Seu lucro é:" + lucro);
 }
-else if(custoDoProduto < 0 || valorDeVenda < 0){
+else if(custoDoProduto <=0 || valorDeVenda <=0){
     console.log("Erro! Digite novamente seu valor");
 }
