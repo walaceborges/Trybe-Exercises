a3e3defd5e7d531bd9c2f128a4f0fa356190b56f
Walace <walacewab@gmail.com>
2021-07-28 21:55:59 -0300

    Adicionar exercicio 9 feito

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/Exercicio-9.js
@@ -0,0 +1,11 @@
+let numero1 = 92;
+let numero2 = 64;
+let numero3 = 31;
+
+
+if (numero1 % 2 === 1 || numero2 % 2 === 1 || numero3 % 2 === 1){
+    console.log(true);
+}
+else{
+    console.log(false);
+}
