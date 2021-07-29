94d3368c37fce8157dc58db1fc3970bcc83e80f7
Walace <walacewab@gmail.com>
2021-07-29 20:15:25 -0300

    Adicionar Exercicio

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/Exercício.js
@@ -0,0 +1,14 @@
+//Exercício 1 
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let list = 0; list < numbers.length; list ++){
+    console.log(numbers[list]);
+}
+
+//Exercício 2
+    let total = 0;
+
+for (let valor = 0; valor < numbers.length; valor++){
+    total = total + numbers[valor]
+    console.log(total);
+}
