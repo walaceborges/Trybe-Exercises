7ca59e7ea8e9f08eba327780ff4ccaaff999b2e0
Walace <walacewab@gmail.com>
2021-07-29 11:38:12 -0300

    Adicionar exercicio 11 em branco e correçoes

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/Exercício-10.js
@@ -0,0 +1,10 @@
+let custoDoProduto = (04 * 0.2);
+let valorDeVenda = 16;
+let lucro = valorDeVenda - custoDoProduto;
+
+if (lucro * 1000){
+    console.log("Seu lucro é:" + lucro);
+}
+else if(custoDoProduto < 0 || valorDeVenda < 0){
+    console.log("Erro! Digite novamente seu valor");
+}
