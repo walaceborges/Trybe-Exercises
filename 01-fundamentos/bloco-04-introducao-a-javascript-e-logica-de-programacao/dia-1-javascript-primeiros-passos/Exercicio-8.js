e4483e718fd818dacd09653418dd3494054b6651
Walace <walacewab@gmail.com>
2021-07-28 21:46:57 -0300

    Adicionar exercicio 8 feito

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/Exercicio-8.js
@@ -0,0 +1,11 @@
+let numero1 = 67;
+let numero2 = 45;
+let numero3 = 22;
+
+
+if (numero1 % 2 === 0 || numero2 % 2 === 0 || numero3 % 2 === 0){
+    console.log(true);
+}
+else{
+    console.log(false);
+}
