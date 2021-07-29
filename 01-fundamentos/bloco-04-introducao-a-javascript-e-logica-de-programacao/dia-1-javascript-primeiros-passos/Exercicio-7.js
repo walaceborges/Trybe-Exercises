a3cb3211d9679329e989ecaefa8600b889c1eab4
Walace <walacewab@gmail.com>
2021-07-28 21:33:40 -0300

    Adicionar exercicio 7 feito

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/Exercicio-7.js
@@ -0,0 +1,23 @@
+let nota = 0;
+
+if (nota >=90){
+    console.log("A");
+}
+else if (nota >=80){
+    console.log("B");
+}
+else if (nota >=70){
+    console.log("C");
+}
+else if (nota >=60){
+    console.log("D");
+}
+else if (nota >=50){
+    console.log("E");
+}
+else if (nota <50){
+    console.log("B");
+}
+else{
+    console.log(false)
+}
