2d4780d4a6365984cb7b051f06d43001794a24c9
Walace <walacewab@gmail.com>
2021-07-30 12:53:14 -0300

    Adicionar Exercicio 5

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/Exercício.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/Exercício.js
@@ -6,9 +6,51 @@ for (let list = 0; list < numbers.length; list ++){
 }
 
 //Exercício 2
-    let total = 0;
+let total = 0;
 
 for (let valor = 0; valor < numbers.length; valor++){
-    total = total + numbers[valor]
-    console.log(total);
+    total = total + numbers[valor];
 }
+console.log("resultado :", total);
+
+//Exercício 3
+let resultado = 0;
+
+for (let valor = 0; valor < numbers.length; valor++){
+    resultado = resultado + numbers[valor];
+    console.log("soma:", resultado);
+}
+let contagem = 0;
+
+for (let elemento = 0; elemento <= numbers.length;  elemento++){
+    contagem  = elemento 
+}
+console.log("contagem: ", contagem) 
+
+let divisao = (resultado / contagem );
+console.log("não aguento mais:" , (divisao));
+
+//Exercício 4
+if (divisao >= 20){
+    console.log("Valor maior que 20");
+}
+else if(divisao <= 20){
+    console.log("Valor menor ou igual a 20");
+}
+
+//Exercício 5
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let armazenamentoMaior = 0;
+let armazenamentoAtual = 0;
+for (let index = 0; index < numbers.length; index++){
+    armazenamentoAtual = numbers[index];
+    if (armazenamentoAtual > armazenamentoMaior){
+        armazenamentoMaior = numbers[index]
+    }
+    
+}
+console.log("Esse numero é o :" , armazenamentoMaior)
+
+
+
+
