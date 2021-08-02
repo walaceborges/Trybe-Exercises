70e117da549154de0050539642817006a93413c7
Walace <walacewab@gmail.com>
2021-08-02 19:08:04 -0300

    Adicionar Exercicio 1.4

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Exercícios -1.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Exercícios -1.js
@@ -19,3 +19,9 @@ console.log(info)
 for (let key in info){
     console.log(key)
 }
+
+// 4 - Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
+
+for (let key in info){
+    console.log(info[key])
+}
