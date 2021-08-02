8222ce636a2dfb7c6c3edc6f9d617e4f3338a9d4
Walace <walacewab@gmail.com>
2021-08-02 18:50:03 -0300

    Adicionar Exercicio 1.2

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Exercícios -1.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Exercícios -1.js
@@ -1,3 +1,5 @@
+// 1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Valor esperado no console:
+
 let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
@@ -5,3 +7,9 @@ let info = {
 };
 
 console.log("Bem-vinda, " + info.personagem)
+
+// 2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. Valor esperado no console:
+
+info.recorrente = 'Sim';
+
+console.log(info)
