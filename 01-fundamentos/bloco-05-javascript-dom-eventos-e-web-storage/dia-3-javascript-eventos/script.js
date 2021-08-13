5c71fe2e24644606967010fe60577fb20bc3bbe8
Walace <walacewab@gmail.com>
2021-08-13 13:51:55 -0300

    Adicionando o exercicio 3

--- 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -64,5 +64,27 @@ function createHolidayButton(buttonName) {
   newButton.id = newButtonID;
   buttonContainer.appendChild(newButton);
 };
-
 createHolidayButton('Feriados');
+
+// Exercício 3:
+// Implemente uma função que adicione ao botão "Feriados" um evento de "click" que muda a cor de fundo dos dias que possuem a classe "holiday" .
+// É interessante que este botão possua também a lógica inversa. Ao ser clicado novamente ele retorna à configuração inicial com a cor "rgb(238,238,238)" .
+
+function displayHolidays() {
+  let getHolidayButton = document.querySelector('#btn-holiday');
+  let getHolidays = document.querySelectorAll('.holiday')
+  let backgroundColor = 'rgb(238,238,238)';
+  let setNewColor = 'white';
+
+  getHolidayButton.addEventListener('click', function() {
+    for (let index = 0; index < getHolidays.length; index += 1) {
+      if (getHolidays[index].style.backgroundColor === setNewColor) {
+        getHolidays[index].style.backgroundColor = backgroundColor;
+      } else {
+        getHolidays[index].style.backgroundColor = setNewColor;
+      }
+    }
+  })
+};
+
+displayHolidays();
