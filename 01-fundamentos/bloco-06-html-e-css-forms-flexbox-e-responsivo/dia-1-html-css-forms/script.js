d701b9f6c5aeacc547c95cb52d266f28da8d3b03
Walace <walacewab@gmail.com>
2021-08-19 22:24:26 -0300

    Adicionando exercicio, segundo passo

--- 01-fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ 01-fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,11 +1,51 @@
-function createState (){
-    let states = document.getElementById('state');
-    let optionStates = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins', 'Distrito Federal'];
-  
-    for (let index = 0; index < optionStates.length; index += 1) {
-      let createOptionState = document.createElement('option');
-      states.appendChild(createOptionState).innerText = optionStates[index];
-      states.appendChild(createOptionState).value = optionStates[index];
-    }
+const date = document.getElementById("data-inicio");
+
+function createState() {
+  let states = document.getElementById("state");
+  let optionStates = [
+    "Acre",
+    "Alagoas",
+    "Amapá",
+    "Amazonas",
+    "Bahia",
+    "Ceará",
+    "Espírito Santo",
+    "Goiás",
+    "Maranhão",
+    "Mato Grosso",
+    "Mato Grosso do Sul",
+    "Minas Gerais",
+    "Pará",
+    "Paraíba",
+    "Paraná",
+    "Pernambuco",
+    "Piauí",
+    "Rio de Janeiro",
+    "Rio Grande do Norte",
+    "Rio Grande do Sul",
+    "Rondônia",
+    "Roraima",
+    "Santa Catarina",
+    "São Paulo",
+    "Sergipe",
+    "Tocantins",
+    "Distrito Federal",
+  ];
+
+  for (let index = 0; index < optionStates.length; index += 1) {
+    let createOptionState = document.createElement("option");
+    states.appendChild(createOptionState).innerText = optionStates[index];
+    states.appendChild(createOptionState).value = optionStates[index];
   }
-createState()
+}
+createState();
+
+function init() {
+  date.DatePickerX.init({
+    format: "dd/mm/yyyy",
+    maxDate: new Date(),
+    weekDayLabels: ["Seg", "Ter", "Qua", "Qui", "Sex", "Sab", "Dom"],
+  });
+}
+
+window.onload = init;
